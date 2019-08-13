a5f37e1bbe038c5b2898244162f60fabdc86834f
sajal.mathurs@gmail.com <sajal.mathurs@gmail.com>
2019-08-13 12:25:06 +0530

    observabe-changes

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { Ingredient } from '../shared/Ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged
+    this.subscription = this.slService.ingredientsChanged
         .subscribe(
           (ingredient: Ingredient[]) => {
             this.ingredients = ingredient;
           }
         )
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
